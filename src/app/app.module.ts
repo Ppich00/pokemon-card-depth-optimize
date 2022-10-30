542ced895dafeb96c3085f609101eadf63de6362
Ppich00 <pichayuth90@gmail.com>
2022-10-30 11:58:57 +0700

    set up angular-universal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PokemonCardDirective } from './directive/pokemon-card.directive';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    PokemonCardDirective
   ],
   providers: [],
   bootstrap: [AppComponent]
